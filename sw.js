1946769c2390f09bf8d2b5ff5a13a79e2a38f547
StrategTheSeer6 <skanda.nagendra@education.nsw.gov.au>
2026-03-11 11:20:51 +1100

    Implement service worker caching for PWA

--- sw.js
+++ sw.js
@@ -1,1 +1,29 @@
+const CACHE_NAME = 'my-pwa-v1';
+const FILES_TO_CACHE = [
+'/',
+'/index.html',
+'/app.js',
+'/manifest.json',
+'https://cdnjs.cloudflare.com/ajax/libs/sql.js/1.8.0/sql-wasm.js'
+'https://cdnjs.cloudflare.com/ajax/libs/sql.js/1.8.0/sql-wasm.wasm'
+];
 
+// Install: Save files to cache
+self.addEventListener('install', (event) => {
+event.waitUntil(
+caches.open(CACHE_NAME).then((cache) => {
+console.log('Caching files');
+return cache.addAll(FILES_TO_CACHE);
+})
+);
+});
+
+// Fetch: Use cached files when offline
+
+self.addEventListener('fetch', (event) =>; {
+event.respondWith(
+caches.match(event.request).then((response) =>; {
+return response || fetch(event.request);
+})
+);
+});
